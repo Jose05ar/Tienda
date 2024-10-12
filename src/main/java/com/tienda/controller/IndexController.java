e0f54a847a3ccffc3abe182d509647f27c78e988
Jose05ar <jose05alvarado.ja@gmail.com>
2024-10-12 17:12:40 -0600

    Avance semana 4

---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,18 @@
+
+package com.tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class IndexController {
+    
+    @RequestMapping("/")
+    public String paginaInicio(Model model) {
+//        model.addAttribute("attribute", "value");
+        return "index";
+    }
+    
+}
