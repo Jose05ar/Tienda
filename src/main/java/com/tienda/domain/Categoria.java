a1ec741b1e5bb67c449a5f93f3695f23df06c61b
Jose05ar <jose05alvarado.ja@gmail.com>
2024-10-26 15:33:34 -0600

    Avance semana 6

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -17,15 +17,15 @@ public class Categoria implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id_categoria")
     private Long idCategoria;
-    private String description;
+    private String descripcion;
     private String rutaImagen;
     private boolean activo;
 
     public Categoria() {
     }
 
-    public Categoria(Long idCategoria, String description, String rutaImagen, boolean activo) {
-        this.description = description;
+    public Categoria(Long idCategoria, String descripcion, String rutaImagen, boolean activo) {
+        this.descripcion = descripcion;
         this.rutaImagen = rutaImagen;
         this.activo = activo;
     }
